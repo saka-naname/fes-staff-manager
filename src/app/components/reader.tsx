38c2d93a5a3a97439e8cf257dfd4821ae1b52036
saka-naname <reisuicupsoup@gmail.com>
2023-10-31 11:17:38 +0900

    Readerの画面を作成

--- src/app/components/reader.tsx
+++ src/app/components/reader.tsx
@@ -6,22 +6,29 @@ import {
   AbsoluteCenter,
   Box,
   Button,
+  HStack,
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
+  useRadioGroup,
   createStandaloneToast,
 } from "@chakra-ui/react";
 
 import { NFCDevice } from "@/lib/device";
 import RCS300 from "@/lib/devices/rcs300";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { sleep } from "@/lib/asyncUtils";
+import { RadioButton } from "./radioButton";
 
 enum TABS_INDEX {
   Uninitialized,
   Initialized,
 }
+
+const stats = [
+  { name: "enter", mainText: "入室", subText: "Enter", statusId: 1 },
+  { name: "exit", mainText: "退室", subText: "Exit", statusId: 0 },
+];
 
 const deviceFilters = [
   {
@@ -46,7 +53,75 @@ const getNFCDeviceInstance = (productId: number): NFCDevice | null => {
 const { ToastContainer, toast } = createStandaloneToast();
 
 export const StudentCardReader = () => {
+  const currentMode = useRef("enter");
   const [tabIndex, setTabIndex] = useState(TABS_INDEX.Uninitialized);
+  const { value, getRadioProps, getRootProps } = useRadioGroup({
+    defaultValue: "enter",
+    onChange: (value) => {
+      currentMode.current = value;
+    },
+  });
+
+  const session = async (device: USBDevice) => {
+    let nfcDevice: NFCDevice;
+    let prevStudentId = "";
+    try {
+      const ndn = getNFCDeviceInstance(device.productId);
+      if (!ndn) {
+        throw new Error("不明なデバイスです");
+      }
+      nfcDevice = ndn;
+
+      await device.open();
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+
+    try {
+      console.debug("selectConfiguration");
+      await device.selectConfiguration(1);
+      console.debug("claimInterface");
+      const deviceInterface = device.configuration!.interfaces.filter(
+        (v) => v.alternate.interfaceClass === 255,
+      )[0];
+      await device.claimInterface(deviceInterface.interfaceNumber);
+      nfcDevice.endpointIn = deviceInterface.alternate.endpoints.filter(
+        (e) => e.direction === "in",
+      )[0].endpointNumber;
+      nfcDevice.endpointOut = deviceInterface.alternate.endpoints.filter(
+        (e) => e.direction === "out",
+      )[0].endpointNumber;
+
+      for (;;) {
+        await nfcDevice.session(device).then((result) => {
+          if (result.success && result.studentCard) {
+            console.log(result);
+            if (result.studentCard.studentId !== prevStudentId) {
+              toast({
+                title: "学生証を読み取りました",
+                description: `学籍番号: ${result.studentCard.studentId}, current: ${currentMode.current}`,
+                status: "success",
+                position: "top",
+              });
+            }
+            prevStudentId = result.studentCard.studentId;
+          } else {
+            prevStudentId = "";
+          }
+        });
+        await sleep(250);
+      }
+    } catch (e) {
+      console.error(e);
+      try {
+        device.close();
+      } catch (e) {
+        console.error(e);
+      }
+      throw e;
+    }
+  };
 
   return (
     <>
@@ -66,7 +141,18 @@ export const StudentCardReader = () => {
                 </Button>
               </TabPanel>
               <TabPanel>
-                <Text>Connected!</Text>
+                <HStack {...getRootProps()}>
+                  {stats.map((s) => {
+                    return (
+                      <RadioButton
+                        key={s.name}
+                        mainText={s.mainText}
+                        subText={s.subText}
+                        {...getRadioProps({ value: s.name })}
+                      />
+                    );
+                  })}
+                </HStack>
               </TabPanel>
             </TabPanels>
           </Tabs>
@@ -86,61 +172,6 @@ const connect = async () => {
     return device;
   } catch (e) {
     console.error(e);
-    throw e;
-  }
-};
-
-const session = async (device: USBDevice) => {
-  let nfcDevice: NFCDevice;
-  try {
-    const ndn = getNFCDeviceInstance(device.productId);
-    if (!ndn) {
-      throw new Error("不明なデバイスです");
-    }
-    nfcDevice = ndn;
-
-    await device.open();
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-
-  try {
-    console.debug("selectConfiguration");
-    await device.selectConfiguration(1);
-    console.debug("claimInterface");
-    const deviceInterface = device.configuration!.interfaces.filter(
-      (v) => v.alternate.interfaceClass === 255,
-    )[0];
-    await device.claimInterface(deviceInterface.interfaceNumber);
-    nfcDevice.endpointIn = deviceInterface.alternate.endpoints.filter(
-      (e) => e.direction === "in",
-    )[0].endpointNumber;
-    nfcDevice.endpointOut = deviceInterface.alternate.endpoints.filter(
-      (e) => e.direction === "out",
-    )[0].endpointNumber;
-
-    for (;;) {
-      await nfcDevice.session(device).then((result) => {
-        if (result.success) {
-          console.log(result);
-          toast({
-            title: "学生証を読み取りました",
-            description: `学籍番号: ${result.studentCard?.studentId}`,
-            status: "success",
-            position: "top",
-          });
-        }
-      });
-      await sleep(500);
-    }
-  } catch (e) {
-    console.error(e);
-    try {
-      device.close();
-    } catch (e) {
-      console.error(e);
-    }
     throw e;
   }
 };

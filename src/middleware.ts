6f241f904ff4ef01ec7a90ad058f460f89314fec
saka-naname <reisuicupsoup@gmail.com>
2023-11-01 06:51:40 +0900

    /reader ページにBasic認証を追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,40 @@ const csrfProtect = csrf({
 });
 
 export async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
   const res = NextResponse.next();
+
   const csrfError = await csrfProtect(req, res);
   if (csrfError) {
     return new NextResponse("Invalid csrf token", {
       status: 403,
     });
+  }
+
+  if (url.pathname.startsWith("/reader")) {
+    const basicAuth = req.headers.get("Authorization");
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(" ")[1];
+      const [user, pwd] = atob(authValue).split(":");
+
+      if (
+        user === process.env.BASIC_AUTH_NAME &&
+        pwd === process.env.BASIC_AUTH_PASSWORD
+      ) {
+        return res;
+      } else {
+        return NextResponse.json(
+          { error: "Invalid credentials" },
+          { headers: { "WWW-Authenticate": "Basic realm='Secure Area'" }, status: 401 }
+        );
+      }
+    }
+
+    return NextResponse.json(
+      { error: "Please enter credentials" },
+      { headers: { "WWW-Authenticate": "Basic realm='Secure Area'" }, status: 401 }
+    );
   }
 
   return res;

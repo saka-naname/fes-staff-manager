06ed8e5012f0d31fc64410d6ceee5207e9b82e16
saka-naname <reisuicupsoup@gmail.com>
2023-11-01 02:03:26 +0900

    Add csrf protection

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import csrf from "edge-csrf";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+const csrfProtect = csrf({
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const csrfError = await csrfProtect(req, res);
+  if (csrfError) {
+    return new NextResponse("Invalid csrf token", {
+      status: 403,
+    });
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+}
